7c3878b4f5d2bd8d695a11647547eb58d56f8100
Lucas cavalcante <lucaskcavalcante@gmail.com>
2026-06-15 14:32:41 -0300

    feat: criação de funções internas para troca de nodes

--- src/minheap.c
+++ src/minheap.c
@@ -14,9 +14,33 @@ typedef struct{
 struct MinHeap_s{
     int tamanho_atual;
     int capacidade;
-    int *pos;
+    int *posicao;
     HeapNode *vetor;
 };
+
+
+/// -Funções internas auxiliares: -///
+static void swapNodes(MinHeap h, int idx1, int idx2){
+   HeapNode temp = h->vetor[idx1];
+   
+   h->vetor[idx1] = h->vetor[idx2];
+   h->vetor[idx2] = temp;
+
+   h->posicao[h->vetor[idx1].vertice_idx] = idx1;
+   h->posicao[h->vetor[idx2].vertice_idx] = idx2;    
+}
+
+
+static void heapifyUp(MinHeap h, int idx){
+    int parent_Idx = (idx - 1) / 2;
+    
+    while(idx > 0 && h->vetor[idx].distancia < h->vetor[parent_Idx].distancia){
+        swapNodes(h, idx, parent_Idx);
+        idx = parent_Idx;
+        parent_Idx = (idx - 1) / 2;
+    }
+}
+
 
 
 MinHeap createMinHeap(int max_vertices){
@@ -27,18 +51,18 @@ MinHeap createMinHeap(int max_vertices){
 
     h->tamanho_atual = 0;
     h->capacidade = max_vertices;
-    h->pos = (int*) malloc(max_vertices * sizeof(int));
+    h->posicao = (int*) malloc(max_vertices * sizeof(int));
     h->vetor = (HeapNode*) malloc(max_vertices * sizeof(HeapNode));
 
-    if(!h->pos || !h->vetor){
-        free(h->pos);
+    if(!h->posicao || !h->vetor){
+        free(h->posicao);
         free(h->vetor);
         free(h);
         return NULL;
     }
 
     for(int i = 0; i < max_vertices; i++){
-        h->pos[i] = -1;
+        h->posicao[i] = -1;
     }
 
     return h;
@@ -50,7 +74,7 @@ void freeMinHeap(MinHeap h){
         return;
     }
 
-    free(h->pos);
+    free(h->posicao);
     free(h->vetor);
     free(h);
     return;
@@ -64,9 +88,7 @@ bool isVertexInHeap(MinHeap h, int vertice_idx){
     if(!h || vertice_idx < 0 || vertice_idx >= h->capacidade){
         return false;
     }else{
-        return (h->pos[vertice_idx] != -1);
+        return (h->posicao[vertice_idx] != -1);
     }
 }
-
-
 
