1f8de43880f944d510c49a310ef75f9b52ea00c8
Lucas cavalcante <lucaskcavalcante@gmail.com>
2026-06-17 17:05:16 -0300

    feat: criação de um módulo para o algoritmo de ddijkstra, utilizando a fila de prioridade implementada anteriormente

---
+++ src/dijkstra.h
@@ -0,0 +1,29 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+
+/**
+ *
+ * @brief Implementação do algoritmo de Dijkstra para cálculo de caminho mínimo.
+ * @details Este módulo utiliza um Min-Heap otimizado para encontrar o caminho 
+ * mais curto entre dois vértices em um grafo, garantindo eficiência O(E log V).
+ */
+
+
+#include "minheap.h"
+#include "list.h"
+#include "graph.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/// @brief Retorna o caminho mais curto entre 2 pontos utilizando o algoritmo de dijkstra
+/// @param g Ponteiro para o grafo analisado
+/// @param origem Ponto de origem
+/// @param destino Ponto de origem
+/// @return Uma lista contendo os vértices do caminho mínimo
+Lista dijkstra(Graph g, int origem, int destino);
+
+
+#endif
+
+
