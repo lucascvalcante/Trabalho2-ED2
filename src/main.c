d0214012dbc73bd8a9754ee14a04ae72a2a80315
Lucas cavalcante <lucaskcavalcante@gmail.com>
2026-07-01 00:12:16 -0300

    feat: criação de uma função estática par cálculo de bounding box
    os svgs estavam saindo muito pequenos, foi criado uma função para cálculo dos limites das quadras e vias para um resultado mais satisfatório

--- src/main.c
+++ src/main.c
@@ -7,131 +7,152 @@
 #include "via.h"
 #include "qry.h"
 #include "exHash.h"
+#include <math.h>
 
-#define MAX_PATH 512
+#define PATH_SIZE 1024
 #define TAMANHO_HASH 1000
 
-static void extrair_nome_base(const char *caminho, char *nome_base) {
-    const char *ultimo_slash = strrchr(caminho, '/');
-    strcpy(nome_base, ultimo_slash ? ultimo_slash + 1 : caminho);
 
-    char *ponto_ext = strrchr(nome_base, '.');
-    if (ponto_ext) {
-        *ponto_ext = '\0';
-    }
-}
+static void calcular_limites_totais(exHash mapa, Graph g, double *min_x, double *min_y, double *max_x, double *max_y) {
+    *min_x = INFINITY;
+    *min_y = INFINITY;
+    *max_x = -INFINITY;
+    *max_y = -INFINITY;
 
-static void montar_caminho(char *path_completo, const char *base_dir, const char *nome_arquivo) {
-    if (base_dir != NULL && strlen(base_dir) > 0) {
-        if (base_dir[strlen(base_dir) - 1] == '/')
-            sprintf(path_completo, "%s%s", base_dir, nome_arquivo);
-        else
-            sprintf(path_completo, "%s/%s", base_dir, nome_arquivo);
-    } else {
-        strcpy(path_completo, nome_arquivo);
-    }
-}
-
-
-int main(int argc, char *argv[]) {
-    char *dir_entrada = NULL;
-    char *dir_saida = NULL;
-    char *arq_geo = NULL;
-    char *arq_via = NULL;
-    char *arq_qry = NULL;
-
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-e") == 0 && i + 1 < argc) dir_entrada = argv[++i];
-        else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) arq_geo = argv[++i];
-        else if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) dir_saida = argv[++i];
-        else if (strcmp(argv[i], "-q") == 0 && i + 1 < argc) arq_qry = argv[++i];
-        else if (strcmp(argv[i], "-v") == 0 && i + 1 < argc) arq_via = argv[++i];
-        else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
-            printf("Uso: ./ted -f <arquivo.geo> -o <dir_saida> [-e <dir_entrada>] [-q <arquivo.qry>] [-v <arquivo.via>]\n");
-            return 0;
-        }
-    }
-
-    if (arq_geo == NULL || dir_saida == NULL) {
-        printf("Erro: Parametros obrigatorios -f <arquivo.geo> ou -o <dir_saida> estao faltando.\n");
-        return 1;
-    }
-
-    char nome_base_geo[MAX_PATH] = "";
-    char nome_base_qry[MAX_PATH] = "";
-    
-    extrair_nome_base(arq_geo, nome_base_geo);
-    if (arq_qry != NULL) {
-        extrair_nome_base(arq_qry, nome_base_qry);
-    }
-
-    char path_geo[MAX_PATH], path_via[MAX_PATH], path_qry[MAX_PATH];
-    montar_caminho(path_geo, dir_entrada, arq_geo);
-    
-    if (arq_via != NULL) montar_caminho(path_via, dir_entrada, arq_via);
-    if (arq_qry != NULL) montar_caminho(path_qry, dir_entrada, arq_qry);
-
-    char path_svg[MAX_PATH * 3], path_txt[MAX_PATH * 3];
-    
-    if (arq_qry != NULL) {
-        snprintf(path_svg, sizeof(path_svg), "%s/%s-%s.svg", dir_saida, nome_base_geo, nome_base_qry);
-        snprintf(path_txt, sizeof(path_txt), "%s/%s-%s.txt", dir_saida, nome_base_geo, nome_base_qry);
-    } else {
-        snprintf(path_svg, sizeof(path_svg), "%s/%s.svg", dir_saida, nome_base_geo);
-        snprintf(path_txt, sizeof(path_txt), "%s/%s.txt", dir_saida, nome_base_geo); 
-    }
-
-    exHash mapa = ler_arquivo_geo(path_geo, TAMANHO_HASH);
-    if (mapa == NULL) {
-        printf("Erro: Nao foi possivel ler o arquivo geo: %s\n", path_geo);
-        return 1;
-    }
-
-    Graph g = NULL;
-    if (arq_via != NULL) {
-        g = ler_caminho_via(path_via);
-        if (g == NULL) {
-            printf("Erro: Nao foi possivel ler o arquivo via: %s\n", path_via);
-            close_exHash(mapa);
-            return 1;
-        }
-    }
-    
-    double min_x = 0.0, min_y = 0.0, max_x = 15000.0, max_y = 15000.0;    
-    FILE *svg = init_svg(path_svg, min_x, min_y, max_x, max_y);
-    if (svg == NULL) {
-        printf("Erro ao criar arquivo SVG: %s\n", path_svg);
-        if (g != NULL) freeGraph(g);
-        close_exHash(mapa);
-        return 1;
+    if (mapa != NULL) {
+        geo_calcular_limites(mapa, min_x, min_y, max_x, max_y);
     }
 
     if (g != NULL) {
+        int num_vertices = getNumVertices(g);
+        for (int i = 0; i < num_vertices; i++) {
+                double vx = getVertexX(g, i);
+                double vy = getVertexY(g, i);
+
+                if (vx < *min_x) *min_x = vx;
+                if (vy < *min_y) *min_y = vy;
+                if (vx > *max_x) *max_x = vx;
+                if (vy > *max_y) *max_y = vy;
+            
+        }
+    }
+    if (*min_x != INFINITY) {
+        *min_x -= 80.0;
+        *min_y -= 80.0;
+        *max_x += 80.0;
+        *max_y += 80.0;
+    } else {
+        *min_x = 0.0; *min_y = 0.0; *max_x = 15000.0; *max_y = 15000.0;
+    }
+}
+
+static void monta_caminho(char* destino, const char* dir, const char* arquivo) {
+    if (dir != NULL && strlen(dir) > 0) {
+        if (dir[strlen(dir) - 1] == '/') sprintf(destino, "%s%s", dir, arquivo);
+        else sprintf(destino, "%s/%s", dir, arquivo);
+    } else {
+        strcpy(destino, arquivo);
+    }
+}
+
+static void extrair_nome_base(const char *caminho, char *nome_base) {
+    const char *ultimo_slash = strrchr(caminho, '/');
+    const char *nome_com_ext = ultimo_slash ? ultimo_slash + 1 : caminho;
+    strcpy(nome_base, nome_com_ext);
+
+    char* ponto_ext = strrchr(nome_base, '.');
+    if (ponto_ext) {
+        *ponto_ext = '\0'; 
+    }
+}
+
+int main(int argc, char *argv[]) {
+    char *dir_in = "";
+    char *dir_out = NULL;
+    char *arq_geo = NULL;
+    char *arq_qry = NULL;
+    char *arq_via = NULL;
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-e") == 0 && i + 1 < argc) dir_in = argv[++i];
+        else if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) dir_out = argv[++i];
+        else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) arq_geo = argv[++i];
+        else if (strcmp(argv[i], "-q") == 0 && i + 1 < argc) arq_qry = argv[++i];
+        else if (strcmp(argv[i], "-v") == 0 && i + 1 < argc) arq_via = argv[++i];
+    }
+
+    if (arq_geo == NULL || dir_out == NULL) {
+        fprintf(stderr, "ERRO: Parametros -f e -o sao obrigatorios!\n");
+        return 1;
+    }
+
+    char nome_base_geo[256];
+    extrair_nome_base(arq_geo, nome_base_geo);
+    char path_geo_completo[PATH_SIZE];
+    monta_caminho(path_geo_completo, dir_in, arq_geo);
+    printf("[*] Processando GEO: %s\n", path_geo_completo);
+    exHash mapa = ler_arquivo_geo(path_geo_completo, TAMANHO_HASH);
+
+    Graph g = NULL;
+    if (arq_via != NULL) {
+        char path_via_completo[PATH_SIZE];
+        monta_caminho(path_via_completo, dir_in, arq_via);
+        
+        printf("[*] Processando VIA: %s\n", path_via_completo);
+        g = ler_caminho_via(path_via_completo);
+    }
+
+    char path_svg_out[PATH_SIZE];
+    char path_txt_out[PATH_SIZE];
+
+    if (arq_qry != NULL) {
+        char nome_base_qry[256];
+        extrair_nome_base(arq_qry, nome_base_qry);
+        snprintf(path_svg_out, PATH_SIZE, "%s/%s-%s.svg", dir_out, nome_base_geo, nome_base_qry);
+        snprintf(path_txt_out, PATH_SIZE, "%s/%s-%s.txt", dir_out, nome_base_geo, nome_base_qry);
+    } else {
+        snprintf(path_svg_out, PATH_SIZE, "%s/%s.svg", dir_out, nome_base_geo);
+    }
+    
+    double min_x, min_y, max_x, max_y;
+    calcular_limites_totais(mapa, g, &min_x, &min_y, &max_x, &max_y);
+
+    printf("[*] Gerando SVG base: %s\n", path_svg_out);
+    FILE *svg = init_svg(path_svg_out, min_x, min_y, max_x, max_y);
+    
+    if (g != NULL && svg != NULL) {
         svg_desenha_mapa_base(svg, g);
     }
 
-    FILE *txt = NULL;
     if (arq_qry != NULL) {
-        txt = fopen(path_txt, "w");
-        if (txt == NULL) {
-            printf("Erro ao criar arquivo TXT: %s\n", path_txt);
-            close_svg(svg);
-            if (g != NULL) freeGraph(g);
-            close_exHash(mapa);
-            return 1;
+        char path_qry_completo[PATH_SIZE];
+        monta_caminho(path_qry_completo, dir_in, arq_qry);
+        
+        printf("[*] Executando consultas QRY: %s\n", path_qry_completo);
+        FILE *txt = fopen(path_txt_out, "w");
+        
+        if (txt != NULL) {
+            processar_qry(path_qry_completo, g, mapa, svg, txt);
+            fclose(txt);
+        } else {
+            fprintf(stderr, "Erro ao criar arquivo TXT: %s\n", path_txt_out);
         }
-
-        processar_qry(path_qry, g, mapa, svg, txt);
     }
+
+    if (svg != NULL) {
+        close_svg(svg);
+    }
+
+    printf("[*] Liberando memoria e finalizando...\n");
     
-    if (txt != NULL) fclose(txt);
-    
-    close_svg(svg);
-    close_exHash(mapa);
+    if (mapa != NULL) {
+        close_exHash(mapa);
+    }
     
     if (g != NULL) {
         freeGraph(g);
     }
 
+    printf("Finalizado com sucesso.\n");
     return 0;
 }
